194cdbd95fe65ed564c32d67b6a94067dd37fe96
Daniel Lapizco Pérez <50065724+daniel-lapizco-perez@users.noreply.github.com>
2023-09-14 14:47:32 -0600

    Tickets GET y POST

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import psycopg2
 
 from flask import Flask, render_template, request, url_for, redirect
 
+from datetime import datetime, timezone
+
 app = Flask(__name__)
 
 def get_connection():
@@ -45,3 +47,41 @@ def create_empleado():
         return redirect(url_for('empleados'))
 
     return render_template('create_empleado.html')
+
+@app.route('/tickets')
+def tickets():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM tickets;')
+    tickets = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('tickets.html', tickets=tickets)
+
+@app.route('/create_ticket/', methods=('GET', 'POST'))
+def create_ticket():
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        departamento = request.form['departamento']
+        status = request.form['status']
+        #creacion = request.form['creacion']
+        creacion = datetime.now (timezone.utc)
+        #actualizacion = request.form['actualizacion']
+        actualizacion = datetime.now (timezone.utc)
+        #cierre = request.form['cierre']
+        cierre = datetime.now (timezone.utc)
+        tecnico = request.form['tecnico']
+        created_by = request.form['created_by']
+        descripcion = request.form['descripcion']
+        porcentaje = request.form['porcentaje']
+
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO tickets (titulo, departamento, status, fecha_apertura, fecha_actualizacion, fecha_cierre, id_tecnico, created_by, descripcion, porcentaaje)'
+                    'VALUES (%s, %s, %s, %s, %s, %s, %i, %i, %s, %i)'
+                    (titulo, departamento, status, creacion, actualizacion, cierre, tecnico, created_by, descripcion, porcentaje))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect('tickets')
+    return render_template('create_ticket.html')
